8a519cefe228b4bd0fabf5f1ed8897972fbce12a
xuananle <lexuanan18102004@gmail.com>
2023-11-30 02:01:26 +0700

    Fix src image link JSX

--- src/client/components/Card.jsx
+++ src/client/components/Card.jsx
@@ -5,8 +5,10 @@ const Card = () => {
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
         <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
+          src={new URL(
+            "abc.jpg",
+            import.meta.url
+          ).toString()} alt="Shoes"
         />
       </figure>
       <div className="card-body">
