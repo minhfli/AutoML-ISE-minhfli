f21884198b533b94e43f2121e48e466ce1731a52
Quannggg <nhieuvai@gmail.com>
2023-12-05 01:56:45 +0700

    Add delete class button logic

--- src/client/app/train/image/page.tsx
+++ src/client/app/train/image/page.tsx
@@ -1,58 +1,61 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 import ClassDivider from "@/components/ClassDivider";
 
 const TrainImage = () => {
-    const [classDividers, setClassDividers] = React.useState([
-        <ClassDivider key={1} title={1} />,
-    ]);
+  const [classDividers, setClassDividers] = React.useState([
+    { key: 1, title: 1 },
+  ]);
 
-    const increaseCount = () => {
-        const newCount = classDividers.length + 1;
-        setClassDividers((prevDividers) => [
-            ...prevDividers,
-            <ClassDivider key={newCount} title={newCount} />,
-        ]);
+  const increaseCount = () => {
+    const newCount = classDividers.length + 1;
+    setClassDividers((prevDividers) => [
+      ...prevDividers,
+      { key: newCount, title: newCount },
+    ]);
+  };
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: any) => {
+      const message =
+        "Are you sure you want to leave? Your changes may be lost.";
+      event.returnValue = message;
+      return message;
     };
 
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
-    const decreaseCount = () => {
-        const newCount = classDividers.length - 1;
-        setClassDividers((prevDividers) => [
-            ...prevDividers,
-            <ClassDivider key={newCount} title={newCount} />,
-        ]);
-    }
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
-    useEffect(() => {
-        const handleBeforeUnload = (event : any) => {
-            const message = "Are you sure you want to leave? Your changes may be lost.";
-            event.returnValue = message;
-            return message;
-        };
-
-        window.addEventListener("beforeunload", handleBeforeUnload);
-
-        return () => {
-            window.removeEventListener("beforeunload", handleBeforeUnload);
-        };
-    }, []);
-
-    return (
-        <>
-            {classDividers.map((classDivider) => (
-                <div key={classDivider.key}>
-                    {classDivider}
-                </div>
-            ))}
-            <button
-                onClick={increaseCount}
-                className="btn btn-ghost flex w-1/4 h-20 card bg-base-300 rounded-box ml-40 mb-5 items-center justify-center"
-            >
-                <h2 className="card-title">Add a Class</h2>
-            </button>
-        </>
-    );
+  return (
+    <div className="mt-auto items-center">
+      <div className="mt-40">
+        {classDividers.map((classDivider) => (
+          <div key={classDivider.key}>
+            <ClassDivider
+              title={classDivider.title}
+              onRemove={() => {
+                setClassDividers((prevDividers) =>
+                  prevDividers.filter(
+                    (divider) => divider.key !== classDivider.key
+                  )
+                );
+              }}
+            />
+          </div>
+        ))}
+        <button
+          onClick={increaseCount}
+          className="btn btn-ghost flex w-1/4 h-20 card bg-base-300 rounded-box ml-40 mb-5 items-center justify-center"
+        >
+          <h2 className="card-title">Add a Class</h2>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TrainImage;
