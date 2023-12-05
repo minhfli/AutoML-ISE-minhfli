9923eb672709db040e787f06c5bce3800c713e30
XuananLe <lexuanan18102004@gmail.com>
2023-12-05 22:50:53 +0700

    Adding Button Upload Folder

--- src/client/components/UploadImage.tsx
+++ src/client/components/UploadImage.tsx
@@ -1,83 +1,35 @@
-// import React, {useRef} from "react";
-//
-// export const UploadImage = () => {
-//     const inputFolder = useRef<HTMLInputElement | null>(null);
-//
-//     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//         const files = event.target.files;
-//
-//         if (files && files.length > 0) {
-//             for (let i = 0; i < files.length; i++) {
-//                 // Access file information
-//                 console.log('File Type:', files[i].type);
-//                 try {
-//                     // Check if the file is an image (you may want to use a more comprehensive check)
-//                     if (files[i].type.startsWith('image/')) {
-//                         // Read the contents of the image file using FileReader
-//                         const reader = new FileReader();
-//
-//                         reader.onload = (e) => {
-//                             // e.target.result contains the base64-encoded image data
-//                             const imageDataUrl = e.target?.result as string;
-//
-//                             // Create an image element for preview
-//                             const image = new Image();
-//                             image.src = imageDataUrl;
-//                             image.style.maxWidth = '1000px'; // Set the maximum width for the preview
-//
-//                             // Append the image to the document or display it in your UI
-//                             document.body.appendChild(image);
-//
-//                         };
-//
-//                         // Read the content of the current file as a data URL
-//                         reader.readAsDataURL(files[i]);
-//                     }
-//
-//                 }
-//                 catch (err) {
-//                     console.error(err)
-//                 }
-//             }
-//
-//             // If you want to access the folder name, you can use the webkitRelativePath property
-//             if (files[0].webkitRelativePath) {
-//                 const folderName = files[0].webkitRelativePath.split('/')[0];
-//                 console.log('Folder Name:', folderName);
-//                 // If end with .jpg then open the file
-//             }
-//         }
-//     };
-//
-//     return (
-//         <input
-//             type="file"
-//             onChange={handleFileChange}
-//             ref={(node) => {
-//                 inputFolder.current = node;
-//
-//                 if (node) {
-//                     ['webkitdirectory', 'directory', 'mozdirectory'].forEach((attr) => {
-//                         node.setAttribute(attr, '');
-//                     });
-//                 }
-//             }}
-//         />
-//     );
-// };
-//
 "use client"
 
-import React from "react";
+import React, { useRef} from "react";
+import Image from "next/image";
+
+interface ImageComponentProps {
+    imageDataUrl: string;
+    index: number;
+}
 
 const UploadImage = () => {
-    const inputFolder = React.useRef<HTMLInputElement | null>(null);
+
+    const inputFolder = useRef<HTMLInputElement>(null);
+
+    const handleButtonClick = () => {
+        if (inputFolder.current) {
+            inputFolder.current.click();
+        }
+    };
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.files);
+        const files = event.target.files;
+
+        if (files && files.length > 0) {
+
+        }
+
     }
+
+
     return (
         <>
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={handleButtonClick}>
                 <div className="flex items-center justify-center">
                     <svg
                         viewBox="0 0 24 24"
@@ -101,27 +53,30 @@ const UploadImage = () => {
                             <line x1="12" y1="3" x2="12" y2="15"></line>
                         </g>
                     </svg>
-                    <input
-                        type="file"
-                        className="file-input"
-                        onChange={handleFileChange}
-                        ref={(node) => {
-                            inputFolder.current = node;
-
-                            if (node) {
-                                ['webkitdirectory', 'directory', 'mozdirectory'].forEach((attr) => {
-                                    node.setAttribute(attr, '');
-                                });
-                            }
-                        }}
-                    />
                 </div>
-
             </button>
+            <input
+                type="file"
+                className="file-input"
+                onChange={handleFileChange}
+                ref={(node) => {
+                    if (node && node !== inputFolder.current) {
+                        // @ts-ignore
+                        inputFolder.current = node;
+                        ['webkitdirectory', 'directory', 'mozdirectory'].forEach((attr) => {
+                            node.setAttribute(attr, '');
+                        });
+                    }
+                }}
+                style={{display: 'none'}}
+            />
         </>
     )
-}
+};
 
 export default UploadImage;
+
+
+
 
 
