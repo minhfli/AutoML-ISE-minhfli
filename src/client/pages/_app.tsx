c99820b6090de58e10567d7deb847ade9992b515
XuananLe <lexuanan18102004@gmail.com>
2023-12-25 00:36:37 +0700

    Add Mock API route + small bug fixed + add Shadcn UI config

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,13 +1,19 @@
 // _app.tsx
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {DevSupport} from "@react-buddy/ide-toolbox-next";
+import {ComponentPreviews, useInitial} from "@/dev";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <main className="scroll-smooth antialiased [font-feature-settings:'ss01']">
-        <Component {...pageProps} />
-      </main>
-  );
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <main className="scroll-smooth antialiased [font-feature-settings:'ss01']">
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <Component {...pageProps} />
+            </DevSupport>
+        </main>
+    );
 }
 
 export default MyApp;
