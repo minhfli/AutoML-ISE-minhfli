0ebe0dfa4cf65584c02ef8626e8de80322c590f9
XuananLe <lexuanan18102004@gmail.com>
2023-12-26 22:19:36 +0700

    Axios + Components

--- src/client/src/app/api/test/route.ts
+++ src/client/src/app/api/test/route.ts
@@ -1,10 +1,21 @@
 import {NextRequest, NextResponse} from "next/server";
 
 export async function GET(request: NextRequest) {
-    console.log("request", request);
+    // console.log("request", request);
     const products = {
         name: "1234",
         price: 100,
     };
+
+    return NextResponse.json(products);
+}
+
+export async function POST(request: NextRequest) {
+    // console.log("request", request);
+    const products = {
+        name: "1234",
+        price: 100,
+    };
+
     return NextResponse.json(products);
 }
