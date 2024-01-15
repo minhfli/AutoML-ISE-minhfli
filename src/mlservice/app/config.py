71b7cc9bdbb0618963d07b1704fcb47bc1a79f6c
XuananLe <lexuanan18102004@gmail.com>
2024-01-16 02:18:14 +0700

    Update .gitignore and package-lock.json, refactor code, and add AutogluonPredictor class

--- src/mlservice/app/config.py
+++ src/mlservice/app/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 config = {
-    "gcpCredentials" : os.getenv("GCP_CREDENTIALS", "../service-account-gcs.json"),
+    "gcpCredentials": os.getenv("GCP_CREDENTIALS", "../service-account-gcs.json"),
 }
--- src/mlservice/app/image_classifier/routes.py
+++ src/mlservice/app/image_classifier/routes.py
@@ -7,13 +7,10 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from contextlib import asynccontextmanager
 
-from google.cloud.storage import Client
-
 from app.image_classifier.autogluon_trainer import AutogluonTrainer
 from app.image_classifier.model import TrainingRequest
-from app.utils import get_storage_client, storage
+from app.utils import storage
 from app.utils.dataset_utils import split_data, create_csv, remove_folders_except, create_folder
-
 
 router = APIRouter()
 
@@ -22,54 +19,70 @@ router = APIRouter()
 # TODO : https://fastapi.tiangolo.com/advanced/events/?h=life  @DuongNam
 @router.post("/api/image_classifier/train", tags=["image_classifier"])
 async def handler(request: TrainingRequest):
-    dataset_path = ""
+    temp_dataset_path = ""
     start = perf_counter()
 
     print("Training request received")
     try:
-        dataset_path = Path(f"/tmp/lexuanan-{uuid.uuid4()}")
+        # temp folder to store dataset and then delete after training
+        temp_dataset_path = Path(f"/tmp/{request.username}/{request.project_name}/datasets.zip")
+        os.makedirs(temp_dataset_path.parent, exist_ok=True)
+
         res = await (storage.download_blob_async(request.username,
-                                                 (f"{request.project_name}/datasets/datasets.zip"),
-                                                 dataset_path))
+                                                 f"{request.project_name}/datasets/datasets.zip",
+                                                 temp_dataset_path))
+
+        print(f"time to download: {perf_counter() - start}")
 
         if res is None or not res:
             raise ValueError("Error in downloading folder")
 
-        # # Có thể dùng /dev/shm để truy cập nhanh hơn /tmp chi sau RAM
-        # # https://stackoverflow.com/questions/9745281/tmp-vs-dev-shm-for-temp-file-storage-on-linux
-        user_dataset_path = f"/tmp/{request.username}/datasets/"
-        #
-        with ZipFile(dataset_path, 'r') as zip_ref:
+        user_dataset_path = f"/tmp/{request.username}/{request.project_name}/datasets"
+        create_folder(Path(user_dataset_path))
+
+        with ZipFile(temp_dataset_path, 'r') as zip_ref:
             zip_ref.extractall(user_dataset_path)
 
-        # # Dcm refactor lại code đc k a Nam =)))
-        #
         split_data(Path(user_dataset_path), f"{user_dataset_path}/split/")
-        # # TODO : User can choose ratio to split data
+
+        # # TODO : User can choose ratio to split data @DuongNam
         # # assume user want to split data into 80% train, 10% val, 10% test
+
         create_csv(Path(f"{user_dataset_path}/split/train"), Path(f"{user_dataset_path}/train.csv"))
         create_csv(Path(f"{user_dataset_path}/split/val"), Path(f"{user_dataset_path}/val.csv"))
         create_csv(Path(f"{user_dataset_path}/split/test"), Path(f"{user_dataset_path}/test.csv"))
-        create_folder(Path(user_dataset_path), "trained_models")
-        #
-        # # delete all folders except split folder to save memory
         remove_folders_except(Path(user_dataset_path), "split")
-        #
-        trainer = AutogluonTrainer(request.request_body)
-        await trainer.train_async(
+
+
+        trainer = AutogluonTrainer(request.training_argument)
+        model = await trainer.train_async(
             Path(f"{user_dataset_path}/train.csv"),
             Path(f"{user_dataset_path}/val.csv"),
             Path(f"{user_dataset_path}/trained_models")
         )
 
+        if model is None:
+            raise ValueError("Error in training model")
 
+        acc = await AutogluonTrainer.evaluate_async(model, Path(f"{user_dataset_path}/test.csv"))
+
+        end = perf_counter()
+
+        return {
+            "status": "success",
+            "message": "Training completed",
+            "accuracy": acc,
+            "time": end - start
+        }
 
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error in downloading or extracting folder: {str(e)}")
     finally:
-        if os.path.exists(dataset_path):
-            os.remove(dataset_path)
+        if os.path.exists(temp_dataset_path):
+            os.remove(temp_dataset_path)
 
-    end = perf_counter()
-    return {"Time to download and extract": end - start}
+
+@router.post("/api/image_classifier/predict", tags=["image_classifier"])
+def predict():
+    pass
